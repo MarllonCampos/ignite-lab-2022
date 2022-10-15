30f30e50981f09b9b5effad5a119aff1d0bc4d92
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-10-14 21:15:34 -0300

    feat: add classname property to all components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,27 @@
 import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-export interface ButtonProps {
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
   const Comp = asChild ? Slot : "button";
   return (
     <Comp
+      {...props}
       className={clsx(
-        "py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-s w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-gray-100"
+        "py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-s w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-gray-100",
+        className
       )}
     >
       {children}
